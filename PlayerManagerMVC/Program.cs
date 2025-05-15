27fc8f1e71ccdfab0bf7ffb46c1330d638413084
SimonXPTO <simaodurao@gmail.com>
2025-05-16 00:54:11 +0100

    Add Controller and Models

---
+++ PlayerManagerMVC/Program.cs
@@ -0,0 +1,15 @@
+﻿using PlayerManagerMVC.Views;
+using PlayerManagerMVC.Controllers;
+
+namespace PlayerManagerMVC
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            PlayerView view = new PlayerView();
+            PlayerController controller = new PlayerController(view);
+            controller.Start();
+        }
+    }
+}
