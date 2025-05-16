bfa09cfa5c9e09248bae676258bd36213abefbc8
SimonXPTO <simaodurao@gmail.com>
2025-05-16 01:10:38 +0100

    Add VIews

---
+++ PlayerManagerMVC2/Views/PlayerView.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using PlayerManagerMVC2.Models;
+
+namespace PlayerManagerMVC2.Views
+{
+    public class PlayerView
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("----\n");
+            Console.WriteLine("1. Insert player");
+            Console.WriteLine("2. List all players");
+            Console.WriteLine("3. List players with score greater than");
+            Console.WriteLine("4. Change player order (currently order is ByScore)");
+            Console.WriteLine("0. Exit\n");
+            Console.Write("Your choice > ");
+        }
+
+        public void DisplayPlayers(IEnumerable<Player> playersToList)
+        {
+            Console.WriteLine("\nList of players");
+            Console.WriteLine("-------------\n");
+
+            foreach (Player p in playersToList)
+            {
+                Console.WriteLine($" -> {p.Name} ({p.Score})");
+            }
+            Console.WriteLine();
+        }
+
+        public (string name, int score) GetNewPlayerInfo()
+        {
+            Console.WriteLine("\nInsert player");
+            Console.WriteLine("-------------\n");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+            return (name, score);
+        }
+
+        public int GetMinimumScore()
+        {
+            Console.Write("\nMinimum score player should have? ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public PlayerOrder GetSortOrder()
+        {
+            Console.WriteLine("Player order");
+            Console.WriteLine("------------");
+            Console.WriteLine($"{(int)PlayerOrder.ByScore}. Order by score");
+            Console.WriteLine($"{(int)PlayerOrder.ByName}. Order by name");
+            Console.WriteLine($"{(int)PlayerOrder.ByNameReverse}. Order by name (reverse)");
+            Console.WriteLine("");
+            Console.Write("> ");
+            return Enum.Parse<PlayerOrder>(Console.ReadLine());
+        }
+
+        public void WaitForKey()
+        {
+            Console.Write("\nPress any key to continue...");
+            try
+            {
+                Console.ReadKey(true);
+            }
+            catch (InvalidOperationException)
+            { 
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void DisplayGoodbye()
+        {
+            Console.WriteLine("Bye!");
+        }
+
+        public void DisplayError(string message)
+        {
+            Console.Error.WriteLine($"\n>>> {message} <<<\n");
+        }
+    }
+}
